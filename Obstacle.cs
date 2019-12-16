c77577a476f5f1eb7d8270af107176a55c437aa5
Caro <45820166+CarolinHollweger@users.noreply.github.com>
2019-12-16 09:43:04 +0100

    renamed unmatching variables
    commit test, herkömmliche Weise nicht möglich

---
+++ Obstacle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+Rigidbody2D rigidbodyObstacle;
+[SerializeField] private float MoveSpeed;
+
+private void Awake()
+{
+    rigidbodyObstacle = GetComponent<Rigidbody2D>();
+}
+
+// Start is called before the first frame update
+void Start()
+{
+        
+}
+
+// Update is called once per frame
+void Update()
+{
+    //if obstacle's position x is < -15f it will be destroyed
+    if(transform.position.x < -15f)
+    {
+        Destroy(gameObject);
+}
+        //if obstacle's position x is < -15f it will be destroyed
+        if (transform.position.x > 15f)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+
+
+private void FixedUpdate()
+{
+    rigidbodyObstacle.velocity = Vector2.left * MoveSpeed;
+}
---
+++ ObstacleSpawner.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public static ObstacleSpawner instance;
+
+    public GameObject[] obstacles;
+
+    public bool isGameOver = false;
+
+    public float minSpawnTime, maxSpawnTime;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Spawn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Spawn()
+    {
+        float waitTime = 1f;
+
+        yield return new WaitForSeconds (waitTime);
+
+        while (!isGameOver)
+        {
+            SpawnObstacle();
+            
+            waitTime = Random.Range(minSpawnTime,maxSpawnTime);
+
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    void SpawnObstacle()
+    {
+        int randomPosition = Random.Range(0,obstacles.Length);
+
+        Instantiate(obstacles[randomPosition],transform.position,Quaternion.identity);
+    }
+}
